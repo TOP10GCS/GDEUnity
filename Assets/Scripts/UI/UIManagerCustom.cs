e5f6ccb8dff89035fe742d7676f6e297e9ca1b30
Kara Ádám <karaadamka@gmail.com>
2026-02-27 23:01:05 +0100

    scene updates

---
+++ Assets/Scripts/UI/UIManagerCustom.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManagerCustom : MonoBehaviour
+{
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
